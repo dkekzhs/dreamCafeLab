f6f0ee9e0011df4bd4108cbf76969991a42913e6
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-09 19:11:02 +0900

    feat : 추가 구 동 상권 별 DB INSERT
    Former-commit-id: c318dfb811bd60639324a2c3dd8b0e4faa42eb3d

---
+++ chatbot/frontend/public/gu.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dac5620819bb5c28bf41b1f5d09fb87ccf82f11125f4d6b0d9763a4f6342e003
+size 1210
---
+++ insertMall.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42e149657caa534870c79db79c24f127e48cab256298965a52175f13fee5e2c0
+size 1164
