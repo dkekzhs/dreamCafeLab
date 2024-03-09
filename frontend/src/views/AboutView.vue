bb0f2785bad3b0a21d50d59514abc3b9cd9ceaea
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-10 03:34:25 +0900

    Feat : add Connect Naver Map to the About page
    Former-commit-id: 4b3baa55526773f59275cfb1d31c21b7bb4fd833

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,9 @@
+<script setup>
+import NaverMap from "@/views/NaverMapView.vue";
+</script>
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <NaverMap/>
   </div>
 </template>
 
