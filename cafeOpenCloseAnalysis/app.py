70063def0be7955e83e3ad5dd9c21a0e880d4213
chohyunwoo <wizardchw@naver.com>
2024-03-10 18:05:48 +0900

    feat: 서울시 카페 개폐업 정보 SRID 0 좌표 기반 DB insert
    Former-commit-id: 90451a6129feca2b2bd4a432836632d7371d6190

--- cafeOpenCloseAnalysis/app.py
+++ cafeOpenCloseAnalysis/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5623d83f28b48cb86d944219321c29029f6b03789fa47205a7b32ab7b5fbac88
-size 850
+oid sha256:3b224aa6a8504d2d499624e06aace57e954df4e058f92843736e393c15873b6b
+size 1162
--- cafeOpenCloseAnalysis/csvAnalysis/filterCSV.py
+++ cafeOpenCloseAnalysis/csvAnalysis/filterCSV.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4128beba49c0aa8a61663a09cbefd19a52c18f5507ff6db9735bcd355fd84007
-size 6572
+oid sha256:08f157b04a582a99654c3999106bc220552c0554c555f7feab5aa4611e17ace4
+size 8090
