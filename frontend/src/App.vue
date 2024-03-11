e9cf1c964f647b7e6447651d3d345699c0aa4465
dkekzhs <dkekzhs5@naver.com>
2024-03-11 18:06:01 +0900

    feat : 모바일 프레임 추가
    Former-commit-id: b9d3da0b437f0e2abc48ac84f5505814f800a62d

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ca6929c5eed31f1fcc6e576ba06b3358e1a9a967f4b3a58782c134328beedc6
-size 1280
+oid sha256:df9236cafb6bbab3f487d0083c1e19f7b529ce343767235d0382ebc462aa91b7
+size 1486
