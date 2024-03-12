37b85199c864208470c7eb33fbdf8e5a1ebdb51c
신시원 <ssw4627@gmail.com>
2024-03-12 17:40:06 +0900

    docs: 집객시설-유동인구 Ridge, Lasso 모델 1차 형성
    Former-commit-id: 9674f87d5ecce4219598faff6ec887a704b6ad0b

---
+++ django/dataAnalysis/settings.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5c0afcc479e129b34078c5a27e46c95861352f391679e745a15da16b42c4ea5
+size 3239
---
+++ django/dataAnalysis/urls.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0dd8857f39365ad4a597b6553a2e0200355360bf22a49c00d6c31f5486fe5248
+size 768
