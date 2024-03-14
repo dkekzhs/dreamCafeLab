ac8b04caf7d69d9e3b130a50eeabf13b1530d088
louis-cho <wizardchw@naver.com>
2024-03-14 12:55:06 +0900

    fe: feat 카드 컴포넌트 update & 디자인 개선
    Former-commit-id: a4c1d5d48f7498be2d29e1340577f598db03aea1

--- frontend/src/views/CardPageView.vue
+++ frontend/src/views/CardPageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28d12e6f156333998ca914202e9b43e84119c76fd0130baab7ec0752d01b9b70
-size 3990
+oid sha256:a93942094517d16ae17abea2a4e0ab2dde808cf975ef9f05296d9f08eda69fec
+size 2714
