d05ee27eb4d0c462ae4751461a7f4b9a71d1656b
dkekzhs <dkekzhs5@naver.com>
2024-03-18 14:54:25 +0900

    fix : 카드뷰 안나오는거 수정
    Former-commit-id: 8dcfb0f5ec1b6e6cd85fb3c4e7377609103c101c

--- frontend/src/views/CardPageView.vue
+++ frontend/src/views/CardPageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f78068fb99d7c88bde7d2daffa149f14d6c77331b01d41d9c88df11eb05cc75
-size 3328
+oid sha256:7889e03ddad9d90ed808ee08eaf874d576536b9404ab813f308c5f35bbd4cbad
+size 3200
