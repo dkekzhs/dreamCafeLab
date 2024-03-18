350d44be94ee45e18e7dd9ade9ccbdb1986126b0
ggramgyo <woghk6761@pusan.ac.kr>
2024-03-18 17:56:43 +0900

    fix: card layout 수정
    Former-commit-id: ab73f483e71aed2ca8909e487967769d9dab664b

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:251910f977fec3c70544323a208496bb7bdf4465508ba79c8fb6a5c7c7cc8b85
-size 1787
+oid sha256:a576463157538d0d34b7f45f70b3c6cabe356064107f90d7c12e45a25f4ddaf7
+size 1806
--- frontend/src/views/CardPageView.vue
+++ frontend/src/views/CardPageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7889e03ddad9d90ed808ee08eaf874d576536b9404ab813f308c5f35bbd4cbad
-size 3200
+oid sha256:1f82564ff93555f58f94b8899e446b886159c227e7950337023433612f6ecb96
+size 3104
