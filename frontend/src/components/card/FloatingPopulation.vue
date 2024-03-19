7f8d7d653a2586e737af98ae76ee77b0ae06ce65
dkekzhs <dkekzhs5@naver.com>
2024-03-19 13:05:35 +0900

    feat: 추가 유동인구 애니메이션
    Former-commit-id: 453af9367a062e2ff02f259e2672319e5e7f2a5c

--- frontend/src/components/card/FloatingPopulation.vue
+++ frontend/src/components/card/FloatingPopulation.vue
@@ -1,28 +1,3 @@
-<!-- Card.vue -->
-<template>
-  <div class="text-h6">{{ title }}</div>
-  <div class="text-subtitle2">{{ author }}</div>
-</template>
-
-<script>
-export default {
-  props: {
-    title: String,
-    author: String,
-    cardColor: {
-      type: String,
-      default: '#FFFFFF'
-    },
-    textColor: {
-      type: String,
-      default: '#000000'
-    }
-  }
-}
-</script>
-
-<style scoped>
-.my-card {
-  width: 100%;
-}
-</style>
+version https://git-lfs.github.com/spec/v1
+oid sha256:af4afb9dd6eec19a7d9f33fe1fa132c0ad973b7e2361f2cb064f7c248a0b240e
+size 1140
