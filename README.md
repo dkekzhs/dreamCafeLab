39ea5a9c8e2bad447b466f6c889f5e0f1d5926b1
김다인 <frambo1se@naver.com>
2024-03-19 23:59:21 +0900

    docs: 240319 일지 작성 Former-commit-id: 4c3121f443fb18cc343b48494366624e563c9750

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9102667a8c458398b07ba9ab02ffe10cee85d15c1e14c46d047e2943aad7fab5
-size 5797
+oid sha256:829e791a5a74e61dafa006661fd98523f32854dc1d282c312d0ec59a38f4256d
+size 5916
--- py/cafe-brand-frcs-stats-2021-2022.py
+++ py/cafe-brand-frcs-stats-2021-2022.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbf869a0f131bbbfe3945e660f18f50ad1042502bb4ddfd2a29326797ce60e49
-size 2193
+oid sha256:ff8f450b7952b0ceab015b6def0560ab6d908dabb14bf1e1f9a177b260b435cd
+size 2194
