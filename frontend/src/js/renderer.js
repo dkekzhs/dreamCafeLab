7347aedbdf9232aee231798874722a89fad4065e
louis-cho <wizardchw@naver.com>
2024-03-20 17:38:06 +0900

    .
    Former-commit-id: ce9ab25ef6e9a86b7c6742c3861bbbccb4093da2

--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4f1d21478a4ab19b0ddb3769b4094c1679f4e0bcc007808e1d81994e8da701e
-size 4982
+oid sha256:ccff88d0195e5766f7cbdb5f71e81ff950afe0ead56674a3388b374501437337
+size 4980
