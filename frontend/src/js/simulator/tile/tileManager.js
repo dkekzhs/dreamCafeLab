a31bb2e849e38c7a3bfd47e6180b3ca633676b0a
김다인 <frambo1se@naver.com>
2024-03-21 00:05:53 +0900

    feat: 타일 초기화 (initTile) 1차 - 도로 배치 제외
    - r*c 영역의 중앙에 cafeSize*cafeSize의 카페 영역 배치
    - 남은 공간에 numOfFacility개에 facility 랜덤 배치
    - 남은 공간에 building, park, emptylot 랜덤 배치
    
    
    Former-commit-id: a55a67e5159532a6bb25f074073b479ff5798dd4

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c408326036adb8132207294e5574d95dbc62d4d35f351784a49dfa789758f14
-size 2345
+oid sha256:6d799f28aa4a35ec05467daf3e07df9f2b6c19f59c64373fa4700b97e6d84d6a
+size 4711
