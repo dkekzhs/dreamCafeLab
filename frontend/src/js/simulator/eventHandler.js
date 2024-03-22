d3d757d7718897926b859e6d5d512e6cf683d023
louis-cho <wizardchw@naver.com>
2024-03-22 13:38:51 +0900

    feat: fe 오브젝트 delete 모드 구현 및 테스트 완료
    Former-commit-id: d3778453fb9cf41ddb676ca785a81a7cf441e829

--- frontend/src/js/simulator/eventHandler.js
+++ frontend/src/js/simulator/eventHandler.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:553fdb50e5cf96ede9a61cc98dfe590571ad1a55bdcc087eabd0ee1f7dee9388
-size 2654
+oid sha256:ca169c0fce197f908c96926ace2ee37b7ca7848cb0724d2be7bbee0edc397806
+size 2837
--- frontend/src/js/simulator/tileSystem.js
+++ frontend/src/js/simulator/tileSystem.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aa0cb241e5e6eaa122ddc117a938fc7d951909d1513b49768983f4b30392374
-size 2793
+oid sha256:3649a08ed23569cec429fd517dfe078d9e5240b1e4a6eb719cad9666aa7b9243
+size 3039
