40a43a51d9f3863426b3fe166dc3948b9925a3a3
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-24 00:15:02 +0900

    feat : 추가 jackson이 못하는 geoConverter
    Former-commit-id: 59e4c714328baf8d663e41a8fa0df1e0285c3cf3

--- backend/src/main/java/com/ssafy/backend/common/utils/GeoConverter.java
+++ backend/src/main/java/com/ssafy/backend/common/utils/GeoConverter.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:832e214c1fcf15534e985136c5dfce8e906f1244db18b1e6b99383f2b966bbf0
-size 947
+oid sha256:8c7da75c4f0887e3574f659c6b6f1d6bbc0f04a59a1a875e2bbb2001ba76ac24
+size 949
