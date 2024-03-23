a01676266c12a7ffad4ef128d11f8c6b7d769db8
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-24 00:09:27 +0900

    fix : SRID 값 0 으로 전부 수정
    Former-commit-id: 3d195f9db489be61cd80886ebca8f47372665742

--- backend/src/main/java/com/ssafy/backend/mall/model/repository/MallRepository.java
+++ backend/src/main/java/com/ssafy/backend/mall/model/repository/MallRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:285bd16b2ffa8c7ec253f6f7a5b040058697deca02d1a6db15d3cce998df9919
-size 2725
+oid sha256:4244e071785f7cf4c02a01413eceff724ab70b72cb96b1c2a797cc6c093668a0
+size 2719
