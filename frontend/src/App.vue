615a2272cd53ea734e719f81f4faa715a13aec6a
ggramgyo <woghk6761@pusan.ac.kr>
2024-03-23 22:43:15 +0900

    feat: Footer 추가
    Former-commit-id: 008e17a85aa759afac8c90b39a7ba6d97449b8ed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3eecf3a5ca1ba2bfc36f6ab888b40d9669035a279661ade90242ea6c93baa0a
-size 1807
+oid sha256:71b2cfeb73ddaaf525b50b3ad2c582439ac91fb190f9792ee794842e71c3f83e
+size 1740
---
+++ frontend/src/views/Footer.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9b1fe3902194a87d048fab4366102a651f9e3a68ad87d9d66a64ecf8a2a470ba
+size 1229
