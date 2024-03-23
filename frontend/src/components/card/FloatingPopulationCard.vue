b947f18b5eb6b1b353046b07a9487c9ffcfe0d2c
ggramgyo <woghk6761@pusan.ac.kr>
2024-03-23 17:13:33 +0900

    style: 유동 인구 애니메이션 적용
    Former-commit-id: ae4f3421dcf72cee13e8c4095c70f163d4c4b49c

--- frontend/src/components/card/FloatingPopulationCard.vue
+++ frontend/src/components/card/FloatingPopulationCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96d30079c43d2da47329e39486cdf762e624ee881509f62ab8fc98020ef1c0bb
-size 1683
+oid sha256:489e15c2ddd10a149f11a5848db6f6cd49dff8da979b7d4597358ed0f64053bc
+size 3392
