ae84b668493e028753906a5e4a8af1ebc5573468
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-24 22:45:23 +0900

    fix : iso 8601 데이트 타임
    Former-commit-id: dbc91ee3d53b6ba6a1cbfb482689ab984f77c597

--- backend/src/main/java/com/ssafy/backend/simulation/model/dto/SimulationCreateRequestDto.java
+++ backend/src/main/java/com/ssafy/backend/simulation/model/dto/SimulationCreateRequestDto.java
@@ -1,3 +1,22 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0fcc544a2c33e86035b1c8194310d4d1fef921f5075f9f5a6266cc05189d0cf2
-size 570
+package com.ssafy.backend.simulation.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class SimulationCreateRequestDto {
+    private double lat,lng;
+    private int radius;
+    private int initialPrice;
+    private double interestRate;
+    private int employees;
+
+    private LocalDateTime startTime;
+
+}
