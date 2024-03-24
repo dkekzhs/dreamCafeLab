f2e705b79ee3d237dd27e81069e0aecdd131b424
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-24 22:30:55 +0900

    fix : 수정 임대료가 전체임대료로 가져오는 버그
    Former-commit-id: 1b8ca1cafde8765d2c89b23723d1e14fd6736ba5

--- backend/src/main/java/com/ssafy/backend/mall/model/repository/MallRepository.java
+++ backend/src/main/java/com/ssafy/backend/mall/model/repository/MallRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4244e071785f7cf4c02a01413eceff724ab70b72cb96b1c2a797cc6c093668a0
-size 2719
+oid sha256:ca49921268f97e832ce0193a4e39ce5a391efd183bdec83b83f9552c527a29fe
+size 2729
