dded4abf78a92b54e2c116c6c850f38063765da0
김다인 <frambo1se@naver.com>
2024-03-25 11:38:25 +0900

    feat: initTile 1차 완료
    Former-commit-id: 8316e8aad3cc66823a1b2d93eb09e7bcecce39b9

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63a1878519fe10e0311f8a502f6ca2dcdc36295779b8f52ec21233861ea19163
-size 9866
+oid sha256:640da4ed3d9445990939826296a90dbe000230b615a6ff37aea866affdda909e
+size 9874
--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a16ae1bdef43bacd9cab17add80cd441d9ca80f6a696e8805349d43c22db3d4c
-size 866
+oid sha256:036f63d348b12fe0161bd7289349863b27ac41e61225677cd6d83d67030b007c
+size 908
