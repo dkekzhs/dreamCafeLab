ae3655435cd194147492e851ff3196867c167fc9
dkekzhs <dkekzhs5@naver.com>
2024-03-25 14:46:09 +0900

    feat : 추가 맵 아이콘 및 셀렉트 버그 수정
    Former-commit-id: a508df06d6c44f8858ea041cfb66f7a3cdb07dd2

--- frontend/src/js/map/mapUtils.js
+++ frontend/src/js/map/mapUtils.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c717f4d47de96448f59571041d941f9400fcaa6e65bf6afe5679e250153e931
-size 4341
+oid sha256:56fea4ffea71bca2d49e3264f4ac844b1a8430bd3b82d507a44ca6dd08e27e31
+size 4718
--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8e18f65fd36f08f05c8be8a7b8a8b8847744bba2d88d29a583f17a0a93e6613
-size 4265
+oid sha256:0d8dc772dc37741590d98831c562f1156dad34e79c84ccbe758d3f718d975079
+size 4210
