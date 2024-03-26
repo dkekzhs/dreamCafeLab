bf8c87508889b3b86a11f9144a2f8de2cf9a908c
dkekzhs <dkekzhs5@naver.com>
2024-03-26 10:59:39 +0900

    feat : 추가 줌크기별 마커 사진변경
    Former-commit-id: 478fcce88a4a2a6426c89b5a58747f59850187c6

--- frontend/src/views/NaverMapView.vue
+++ frontend/src/views/NaverMapView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cefeb43d24e5eff8b913312f1a2ed119ca1fb8643d8af03c26a4ace577a8a0f
-size 10057
+oid sha256:e25798ba85e17c07ffb5331cf7e8660c859ed94f5cf3d328530df7f1e2eda324
+size 10121
