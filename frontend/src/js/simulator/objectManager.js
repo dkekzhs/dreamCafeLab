686d5f2d36b5e468196cb0e092520637b9499ea7
김다인 <frambo1se@naver.com>
2024-03-28 01:12:20 +0900

    feat: objectManager - tileManager 연결
    Former-commit-id: 2b241a66161cd80df246e3316202e8c7bc0cd70b

--- frontend/src/js/simulator/objectManager.js
+++ frontend/src/js/simulator/objectManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6111e7bd0e46ef8c6bb2af6bcc5a0252157de7cab3c3e10368f3664c5fc42b04
-size 5723
+oid sha256:3e8b0cdcde933675d74cace35c5e0f596949174e9659269c6e981bd42f9c7690
+size 7307
