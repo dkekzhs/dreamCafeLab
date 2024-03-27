1ef417adb7fc63ee094bff6d611856ac94f26c9a
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-03-27 13:26:01 +0900

    feat: fe asset 추가
    Former-commit-id: d8440eb6f611550d0f957454eb53edc63cb5187b

--- frontend/src/js/simulator/models.js
+++ frontend/src/js/simulator/models.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fae27e2bcde3c81aa8489d1a68343e7ad7135c67c3a6768c1c93e9e50aa80eaa
-size 2673
+oid sha256:386bc848038e20d459193a24ac0f93a25adba292a6bdbfb70b0155fe164c7b46
+size 2523
