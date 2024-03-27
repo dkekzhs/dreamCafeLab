10ba42b0e675e20648a4d32a02131d23769fac9c
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-03-27 10:59:17 +0900

    feat : fe simulator asset 재정비
    Former-commit-id: b84f4114ad10ef72413c05ab2ae9ea35e65d4c0e

--- frontend/src/js/simulator/models.js
+++ frontend/src/js/simulator/models.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f97f5c61dd6f985358a9d6fdda66fca2921be85d8f33225e5b1b78400381885b
-size 2902
+oid sha256:099915abb81a27bb6ca2d53a6c8d7254f0dbcef4165e6ae1c9cc20624b061f49
+size 3739
