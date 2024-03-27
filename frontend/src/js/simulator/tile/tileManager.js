df140d331a7201dd6e5ad0a2250ebd08c0815214
김다인 <frambo1se@naver.com>
2024-03-27 21:02:28 +0900

    fix: addClerk 오류 수정
    Former-commit-id: 0935a104109edbf8aa6bf06f05707ad3ef0c9131

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dfe77b47bd67d20b01524944ce9469f307b3b0ca7566ce2a59eb89d3c5f06cd
-size 15161
+oid sha256:d9ffdd9a334fc96e73bfa18af267856d8453fe2ee1cb95f8fced66e09a0e9e15
+size 15146
