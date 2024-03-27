f274212a9a353e66540a184e15bc2dba8eaa5dde
ggramgyo <woghk6761@pusan.ac.kr>
2024-03-27 17:48:53 +0900

    style: layout 조정
    Former-commit-id: 0753e10674dd8af6481bd3de75cc6e2de65854ad [formerly 6d33e3e47ae93a6ccbf299cacf44770c7972efcb]
    Former-commit-id: 35cd36a9604f8197f92fe1d8fb32ab2df6e8b4ef

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b532c978a9847f7e7f9b76dcecd84518316e5e776817d935158ac5b0366df488
-size 791
+oid sha256:5c2694a49985863e80bbbd743fa50a401c8abea5efaab98ad88e585c47ccece3
+size 800
--- frontend/src/views/CardPageView.vue
+++ frontend/src/views/CardPageView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e11b33ca26150c253185020871ffa6f8a1d44962fdfa5bb2b4fcc7729dd80e2
-size 5531
+oid sha256:6e3ee630d1dd9687a7aa03aac6b4bf9995e01b7f05fa0e02c2e5ab8e1737c971
+size 5617
