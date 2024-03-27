32b7fee7355a56f3b9dc3694609714d3fc8aa6ce
kseenyoung <kseenyoung@gmail.com>
2024-03-27 15:43:38 +0900

    feat: 가게 정보 카드 누르면 모달
    Former-commit-id: ce8dda47acc5c15ef7c819b5b5872300695d9c49

--- frontend/src/components/SimulationReport/StoreInfo/StoreInfoCard.vue
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228a5e48b6ea9d5d856f87b27ad876b98b9f61be9084730307d795d1297fa982
-size 1125
+oid sha256:5ca9a85f019a209bace528edb83912539afba92111baa575075ec9e43aaa8c2f
+size 1299
---
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoCardDetail.vue
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9391f8746dc511f88346a0a1c9ca26f5456c189fb264159f914c61e863d6642
+size 1507
