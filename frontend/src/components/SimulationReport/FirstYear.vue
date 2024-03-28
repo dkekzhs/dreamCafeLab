25422f0964b36c3ddaff202839ec35651962716e
kseenyoung <kseenyoung@gmail.com>
2024-03-28 11:34:30 +0900

    feat: 재무제표 차트 추가
    Former-commit-id: 00e1baca643a2d50369f728cab39587ac91d01e6

--- frontend/src/components/SimulationReport/FirstYear.vue
+++ frontend/src/components/SimulationReport/FirstYear.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b29240e2ddc6c1b343e27fb61bd6bddbc675e721e6a6fc2862b719760694036
-size 728
+oid sha256:05f9cbd94fb5b9bd9cfcf1b959494fae0182d367ac0379d20cd44ebd9f46506c
+size 729
--- frontend/src/views/SimulationReport/SimulationPolyFile.vue
+++ frontend/src/views/SimulationReport/SimulationPolyFile.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0a584f1add0f752989586a5f80dac8f02f60b9af22804e874227000652ff076
-size 2927
+oid sha256:df97ebc61132ed1cbe67b0c920c5871859b5e979c93b5d9a690f915551c82227
+size 2839
