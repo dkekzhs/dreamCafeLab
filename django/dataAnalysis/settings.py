1aca639b78604f6f5f4b2efe9cb120ace480ff08
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-28 21:30:27 +0900

    feat : 추가 장고 도커파일 배포 cors 설정
    Former-commit-id: 90fb233b9caa97fc0baa4a9d96d2b689d0dacf4a

--- django/dataAnalysis/settings.py
+++ django/dataAnalysis/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84fbf45d3a20614cc558015acb5dae5e6083cb42912b35d190bece3b32384c8d
-size 3936
+oid sha256:38e003edd2e59136a58a27da15a87453b50f53ebd5349edcb11647022d51eba5
+size 4066
