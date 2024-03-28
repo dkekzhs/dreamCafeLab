65f33a461f3eb52604cd24dfa0ed5cf668ffedf6
김다인 <frambo1se@naver.com>
2024-03-28 17:02:31 +0900

    feat: facilityList에서 집객시설 id 함께 관리
    Former-commit-id: 39d249ea4e3f37d344d7250a030a9653d993aef6

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:610c2bbfaab9a3f455974cb84d29c9be3b819a721d0d0441101a29377a0bc136
-size 15810
+oid sha256:35e179b99fd9ab795b92534fbb0daa5f8047c9f62f64a92efb1e4f338768240f
+size 15867
--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f88ed87f372abefe65250c46985b7210c7a549ae0649ff4607e0ab35eca2773f
-size 1938
+oid sha256:630ec3a1d9fdb76e72f37d9940f313f62a184896c100e7ef6b04ff4b1602532d
+size 2234
