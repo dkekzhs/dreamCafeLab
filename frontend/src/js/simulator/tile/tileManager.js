b92569e20f343741eebb7cafe5de95b4c0133687
김다인 <frambo1se@naver.com>
2024-03-28 15:33:55 +0900

    feat: cafe 영역의 clerk 위치 배치 로직 수정
    Former-commit-id: 12c8454c7bfa72ec761c9b4f0e9413e166c7ca3a

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73578635c25001bae4434b11455b954254472e7e8c503a9c35cac750c90359d2
-size 15435
+oid sha256:3462357a1518e38f4aed1f01fd570ead0e658f5a6eb546cb92fe3feb6a6e342b
+size 15283
--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce6874798cfaee33db5f51d8c4da8f71ddd530da82ff549338290030faf6fb6f
-size 1908
+oid sha256:f88ed87f372abefe65250c46985b7210c7a549ae0649ff4607e0ab35eca2773f
+size 1938
