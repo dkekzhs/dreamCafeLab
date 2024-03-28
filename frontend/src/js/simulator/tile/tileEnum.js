e42574b5085605b968e8b418a9aa8c80381b2608
김다인 <frambo1se@naver.com>
2024-03-28 09:59:09 +0900

    fix: tileEnum의 object별 subType 값 수정
    Former-commit-id: 79626d768eb14bc98b593a21df1a027f84efd8ff

--- frontend/src/js/simulator/tile/tileEnum.js
+++ frontend/src/js/simulator/tile/tileEnum.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2a1d36c06e57be26cc2c149cfd58e23b4e1c537c0f245bc56c7e7a864ca3171
+oid sha256:cae79db3ec33010c3d183e48991c6477f4d4b6b87883e79e6f12044166c5e154
 size 1212
