c2fbd3267b87324b23965841a1a5a0593e2cdbf4
김다인 <frambo1se@naver.com>
2024-03-28 23:50:59 +0900

    feat: 집객시설 설치 시 facilityList 반영, nearFacilities 형태 롤백
    Former-commit-id: f9bcf955dee7b2e16560d4d49c54e431b85f0a59

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c2ad618d6de4252c80e87b0679ba62c110b546ec4e0fd01db93b2d36a0cf632
-size 16595
+oid sha256:af74ce01a8c5d035bfb1bb0f8314bc7ac91431e151c8612d99223e4a3570008e
+size 16467
--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e36526c5b6f180038f1ac786e9fd9da75340adb8ff61582b7e76772f83bac5b1
-size 2392
+oid sha256:34c59ef8f52e6b3819fc8f37f1c35938969979a695f461863b946f8ed1af6e51
+size 2232
