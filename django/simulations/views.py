6b62f20b820c359e30dee45255d879e1efade891
신시원 <ssw4627@gmail.com>
2024-03-28 17:59:24 +0900

    feat: 시뮬레이션 모델 구현
    Former-commit-id: ff436b574265ea08be4da08435b7f3c75fb3c690 [formerly f355133881eb4d9253d98878983048554cf38c47 [formerly d4d7b62b440e1bcbf62de4ed2cea1157e7d8e351]]
    Former-commit-id: b2140c7a0339231316c8c07cf190994105e1b885
    Former-commit-id: 46e37953f1e9597837142a04a8029dfed04710e7

--- django/simulations/views.py
+++ django/simulations/views.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b352b6b4534eaaeb31e5968d628f7694444af7e29dddcf360083de45c258c9b
-size 1634
+oid sha256:6b07540822fa20979337c73f6515d9328a379e3a928db93b508d282d742c37ca
+size 10990
