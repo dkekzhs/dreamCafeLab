7f8b02820de8c967bdf0354f4b555e76bd1811e7
김다인 <frambo1se@naver.com>
2024-03-28 18:04:09 +0900

    fix: 집객시설 삭제 시 facilityList 반영/nearFacilities 수정
    - deleteCityObject에서 facility 삭제 시, facilityList에서도 제거
    - nearFacilities 리스트의 형태 수정 (dist>distance, id>facilityId, type>facilityType)
    
    
    Former-commit-id: 0329fdb5d7c9f3c938abca2b00b6b017f575f775

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35e179b99fd9ab795b92534fbb0daa5f8047c9f62f64a92efb1e4f338768240f
-size 15867
+oid sha256:7c2ad618d6de4252c80e87b0679ba62c110b546ec4e0fd01db93b2d36a0cf632
+size 16595
--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:630ec3a1d9fdb76e72f37d9940f313f62a184896c100e7ef6b04ff4b1602532d
-size 2234
+oid sha256:e36526c5b6f180038f1ac786e9fd9da75340adb8ff61582b7e76772f83bac5b1
+size 2392
