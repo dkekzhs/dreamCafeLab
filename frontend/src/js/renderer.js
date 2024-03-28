a07d9af99f1ca9118fad7f10fd9df023ea22efbc
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-03-28 11:33:31 +0900

    feat : fe 날짜 시스템 도입 및 잔고 갱신 로직 추가
    Former-commit-id: 8752429611eeecb7d1923b01ec161ecc1fb5a98b

--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f74fe25d462ada5791ea6fa865d859318fa882c81964a6ce8d8c1362c87d9dd3
-size 6292
+oid sha256:b826749b6b68c69605d8133c367f65c31c940cc881e9adac861b9f56906536a8
+size 7272
