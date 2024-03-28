5bbee57893d146e10c90b8586ebaa78dfc4e3ac3
김다인 <frambo1se@naver.com>
2024-03-28 10:16:05 +0900

    fix: 사용자 입력에 따른 facility 설치 동작 확인
    Former-commit-id: 050962a7289cd020dfeedcfc7cc053bd73cddea7

--- frontend/src/js/simulator/objectManager.js
+++ frontend/src/js/simulator/objectManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e8b0cdcde933675d74cace35c5e0f596949174e9659269c6e981bd42f9c7690
-size 7307
+oid sha256:b50e7b563f2cd0a888ebc24dabbda7d4744e044963041e52ddf9969f38e14c1b
+size 7331
