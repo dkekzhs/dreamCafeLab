d3b3930fd3c12549c8056e5dcb79915178f31c54
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-03-28 10:24:20 +0900

    feat : fe play, pause
    Former-commit-id: 891955bc50f58cad37098272aa2cffd2780805ff

--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f03feefbfd72ec4f475690c014d7f3cb4c28a1cd4216c4d5b06d0c95513c506
-size 6254
+oid sha256:f74fe25d462ada5791ea6fa865d859318fa882c81964a6ce8d8c1362c87d9dd3
+size 6292
