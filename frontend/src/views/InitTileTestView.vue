97d1c647c381d7f1cb0da39c7f45f1028fc4a413
김다인 <frambo1se@naver.com>
2024-03-29 14:07:49 +0900

    feat: 집객시설 추가 시 기존 facilityList 상태 반영한 dist 부여
    Former-commit-id: 1e23da2b928dc8e517fe240ae9e3bdb91af5208b

--- frontend/src/views/InitTileTestView.vue
+++ frontend/src/views/InitTileTestView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34c59ef8f52e6b3819fc8f37f1c35938969979a695f461863b946f8ed1af6e51
-size 2232
+oid sha256:946e65ca0e65231dc073ffac190e4ff00571913c03e4f706ef651ed00d5aa523
+size 2248
