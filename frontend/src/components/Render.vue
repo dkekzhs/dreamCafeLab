167cb7e1a96b5f251e5a14e1f50714c08cbf8845
김다인 <frambo1se@naver.com>
2024-03-29 14:31:54 +0900

    fix: sim_test 페이지에 openDialog 버튼 추가
    Former-commit-id: 7b15c62f9c70fb63d21097dd23bb35810316ebd8

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6f3d9abd32e42730bdd2b6ec75104adf99af79bdb5b62899736a2f44c786e8a
-size 770
+oid sha256:5ac0a089c50967e9735eec0dadcd202bcddde2f152f5ffd9d7e45847b5703487
+size 884
