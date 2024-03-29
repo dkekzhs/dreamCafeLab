89f1180d7d3cb4c5aa885a28b2bf9aa1d12ba93b
dkekzhs <dkekzhs5@naver.com>
2024-03-29 09:22:37 +0900

    feat : 시뮬맵 요청 연결
    Former-commit-id: ab86286a6e463f0ccce11907d71e6e7f38b45511

--- backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67063b5afd99dced1f52b28350f53d94bf9ea2cc0db519f9492e40f8bf3960dc
-size 8291
+oid sha256:3cecdcf5b5cd17475e5493562ae074ddb58267e27e376c6b6f76c92da7899ded
+size 8513
