250ccd960be8227e8b596b3f26e996fb1816ec15
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-03-29 14:37:59 +0900

    feat: fe 세부 scene 장식 요소 추가
    Former-commit-id: 78db5ab6ffd469fe9aaf2c0ea973cf31080fdd77

--- frontend/src/js/simulator/objectManager.js
+++ frontend/src/js/simulator/objectManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:967aacc3aec4bb8717b2c0c1c0f07fdfc3b5e22611dcf638f07df0d3e265a7f4
-size 8386
+oid sha256:9e45857c43227fd1118c1eea205ebe6b1aa84ef0db171607837749ad9a5ec96e
+size 8540
