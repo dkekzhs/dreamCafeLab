fd38e6e3a62210b6e80469722f2a6393023a4e5a
dkekzhs <dkekzhs5@naver.com>
2024-03-29 17:55:38 +0900

    feat : 추가 기본 자금 수정중
    Former-commit-id: 37c2cc728088559f551fc90c03720aab7f089ee2

--- backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cecdcf5b5cd17475e5493562ae074ddb58267e27e376c6b6f76c92da7899ded
-size 8513
+oid sha256:8e65b00e57abc391b763a11f0b35c6ba89a3f85fccf58e2c5b19ea45922c63f4
+size 10140
