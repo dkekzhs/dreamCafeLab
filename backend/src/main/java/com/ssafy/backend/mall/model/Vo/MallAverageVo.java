2bbd614b9da3c82ffd34644d7a8a6a22bd30ed20
dkekzhs <dkekzhs5@naver.com>
2024-03-29 15:12:00 +0900

    feat : 추가 카페 브랜드 창업비용 계산 수정
    Former-commit-id: b43e9888b5fd34d3c1ae1ac6949f291680515d0b

--- backend/src/main/java/com/ssafy/backend/mall/model/Vo/MallAverageVo.java
+++ backend/src/main/java/com/ssafy/backend/mall/model/Vo/MallAverageVo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a78e039dcb5d6bfcaa0876a4e266c33691b6c23e0e409ce529027819cad9b951
+oid sha256:f081356774e66752765eefd6df3a47aecf49105a1fcbe6053658d74f5ca8fe8b
 size 543
--- backend/src/main/java/com/ssafy/backend/simulation/service/SimulationFacadeImpl.java
+++ backend/src/main/java/com/ssafy/backend/simulation/service/SimulationFacadeImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c37bcfce2593e869fb4d4cd63f7a09dc06559b88da9b05a7af50da79e69fb472
-size 3203
+oid sha256:d6d53a0e312a089a6aa1c66eb405e375325771d9a5c94587a05902665c896088
+size 3307
