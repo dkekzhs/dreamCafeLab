fbeded965c62f38f0e46f6210b9bcd6e98268068
kseenyoung <kseenyoung@gmail.com>
2024-03-31 00:32:30 +0900

    feat: 투자대비 이익 디테일 구성
    Former-commit-id: 810229b6c633a9632fc982da01fa932710d8771a [formerly 06add7197b058eded18920b3ca1a968d1650ec80 [formerly e6adcb37fe8876894bc02e8b2c55d198fbcda5ec]]
    Former-commit-id: 4f4f09833e297257956563d42bd22e76880f7dda
    Former-commit-id: 4669e251f7a496eb4bc28d6424c12662c0772a02

--- frontend/src/components/SimulationReport/StoreInfo/StoreInfoBasic.vue
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoBasic.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce5335af4ff5ae0875014125138e5608734bf4a1ada2f1787b1c3ef89e9b523
-size 1445
+oid sha256:3f8f43333a89a1452d506f6c492bf05020afc758ae2fb0aea37fe8f03e0477c9
+size 2595
