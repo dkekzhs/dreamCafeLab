2c1857dd7574e3864bf9a4cd3e57888149d7b383
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-03-30 16:02:47 +0900

    feat : 추가 시뮬레이션 업데이트 기능
    Former-commit-id: 868ada92c777ddefa8b827f69fa9ffdc567dd1d2

--- backend/src/main/java/com/ssafy/backend/mall/model/Vo/MallAverageVo.java
+++ backend/src/main/java/com/ssafy/backend/mall/model/Vo/MallAverageVo.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f081356774e66752765eefd6df3a47aecf49105a1fcbe6053658d74f5ca8fe8b
-size 543
+oid sha256:aa4b1c4867c15b428bb9aa0cf8018f45338c901c35e62d1903e223234dc5a97e
+size 539
