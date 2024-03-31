e8f95b81e74602df2458ddc79ed61f43dd5a4c00
김다인 <frambo1se@naver.com>
2024-04-01 01:24:53 +0900

    fix: addClerk/removeClerk 수정
    Former-commit-id: e5a3484a1fdf74b86a869b5cbcec394494a0e507

--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc0ab8ee106417ad94d7182555583ba84b56c361598bfa568369d8a7be76e9ec
-size 18420
+oid sha256:bbc754bb8861b2bc9cef7681b9fb668e7e963001e6237cb7dc2348912311fcf4
+size 19583
