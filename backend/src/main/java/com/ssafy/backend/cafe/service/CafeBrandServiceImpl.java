15038cc08527cf5f73e20e3961f8eca9089445ab
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-01 03:18:13 +0900

    feat : 추가 맵에서 동으로 1등 추천 카페 가져오기
    Former-commit-id: 1449052ece21f38c5d7cc559a069a90d178ea173 [formerly 27227d10a28c392b072a9c516e704e652707e9d1 [formerly 36f17da0a5b2f6ac44aefd1bf7441213e2f4c40b]]
    Former-commit-id: 863be4a6faa64f3b377b61a97484d222f6f7b3cd
    Former-commit-id: 06849e2b4ceb3cf9b21278ca354b307d37eed284

--- backend/src/main/java/com/ssafy/backend/cafe/service/CafeBrandServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/cafe/service/CafeBrandServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31fbbe6c1f515fa701496f291b96c9c0e75465bf0f64237058640df505331af3
-size 4488
+oid sha256:c0efe6c7f0c44a2d3c7e074f8721e6133d05414fdd91f7d360aaa3d79cf8938a
+size 4916
