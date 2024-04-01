3d6afbe612abf3cf6a2240781a84b9af614c1ca3
김다인 <frambo1se@naver.com>
2024-04-01 13:51:25 +0900

    feat: 집객시설 타입별 개수 리스트 관리/직원,집객시설 업데이트 로그 관리
    Former-commit-id: 54cee13ca3611b40b0dc8da7495449dc5a7a9b14 [formerly a81025b9037e5fac30ec2179e6e3e9effcf8573e [formerly 9c94409bb04da77593476da12bdbc625a014973c]]
    Former-commit-id: 986a9f64792c1a5e1034383fdc3627c48476bf72
    Former-commit-id: 7e69246d6f912f9810d402f85f4b592244e14757

--- frontend/src/js/simulator/tile/tileEnum.js
+++ frontend/src/js/simulator/tile/tileEnum.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4d8569bd06948e0bfb93d970233c1067db04cddf14432f43c7f1ba85198724c
-size 1235
+oid sha256:dd5cd0d9a673b7bf883e25d12315779c89527ba7c94cccbe9036ad8013566174
+size 2159
--- frontend/src/js/simulator/tile/tileManager.js
+++ frontend/src/js/simulator/tile/tileManager.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbc754bb8861b2bc9cef7681b9fb668e7e963001e6237cb7dc2348912311fcf4
-size 19583
+oid sha256:dd61e1598cb0c4a16f8a9a85ad055c9fe19d1b2d2318780f7a8db28e32ff18aa
+size 23465
