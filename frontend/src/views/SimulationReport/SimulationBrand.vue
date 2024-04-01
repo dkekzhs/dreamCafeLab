aa90a8847393838890d0dbcbe63fe66e537a281f
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-01 09:40:57 +0900

    style: 전체적인 css 조정
    Former-commit-id: fdd2da5e4ddcad1cd432ed413113565e62f4abb4 [formerly 9d0b1810e8f44b7b257bfb386b3162f4769b04f2 [formerly 9aac0d0b11658ebf941466b799a5770c51f5ee74]]
    Former-commit-id: f958966e32307004766deb960f7dcc740def8ac9
    Former-commit-id: 87e7a011a807f744031c63345953a4801f977c64

--- frontend/src/views/SimulationReport/SimulationBrand.vue
+++ frontend/src/views/SimulationReport/SimulationBrand.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a127566f786d8aab0b14a6eb1d519e4aebcb8b4d5e74ae3b160ccb9a7960f26e
-size 4748
+oid sha256:cb8ee64d5971d551d6a7fe57fa24c2e0bdff6fa5e9b89e3c9fc03a97010b881e
+size 5562
