977c130c69640d62638d8fdc0af6f742009bf016
dkekzhs <dkekzhs5@naver.com>
2024-04-01 13:23:04 +0900

    feat : 추가 맵 카페 정보 연동
    Former-commit-id: 1e2e2775afc225790266e1e7fe3b6b259db36eba

--- frontend/src/views/NaverMapView.vue
+++ frontend/src/views/NaverMapView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ebca79aef3f57a907de65238ef97295accbdd9b6172fedb186b32594f3cc27a8
-size 13524
+oid sha256:2ebc5c87bb9ec100cd7938321eac094853fd1f39fce376c33e7f78adbce0282b
+size 14875
