5a0569a1bad559d76233861070ff57f4ace6ab61
dkekzhs <dkekzhs5@naver.com>
2024-04-01 13:40:18 +0900

    feat : 추가 시뮬레이션 위치 선택 상권 위치 기능 + 500미터 수정
    Former-commit-id: c365883bb1c42823b77895cfd33b7b19a1bff979

--- frontend/src/views/SimulationReport/SimulationMap.vue
+++ frontend/src/views/SimulationReport/SimulationMap.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8108c3c365b4d8dff9922a6edbd657339291f2d77e5a86355d9d17c4e177e618
-size 4863
+oid sha256:8abfb7d448379eb19109118c0c842744d0a09fa61ea081fe910296de27cea312
+size 4862
