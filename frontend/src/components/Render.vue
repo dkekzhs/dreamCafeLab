25892edb80e450cffbac5a26018b221ab130dd76
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-01 23:44:52 +0900

    feat fe : 렌더러 시간 조정 및 store 이용
    Former-commit-id: 7c6ff71b692b2c0bd9a00ca231944304f5c2aff6

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2ab02662dbcb502c88c2ec19be318e20f5c32c71fcd4e65b131b194a6b63509
-size 1194
+oid sha256:87bb09b6a64eeb25a7bdd8ab67c928a2dee044f5440ef78bf6d138c69db4fb53
+size 1569
