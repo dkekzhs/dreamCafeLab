9d0b1810e8f44b7b257bfb386b3162f4769b04f2
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-01 09:40:57 +0900

    style: 전체적인 css 조정
    Former-commit-id: 9aac0d0b11658ebf941466b799a5770c51f5ee74

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e1c56583329e5faec0c41d2814d5bc41582a76c1c9bdcb2b7fc5aa073475c4b
-size 1062
+oid sha256:771cb61af1242004dd4ee8dd80d20ea20c41b0c7863a0f675c705fb92af8fe67
+size 1192
--- frontend/src/views/SimulationReport/SimulationAsset.vue
+++ frontend/src/views/SimulationReport/SimulationAsset.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1162c0f3390a6b9876a718fb101313dcf0b8e00f55448cd0660c49410e10eb0a
-size 5622
+oid sha256:82529055ff6a75ec0eb7581fad333d55b4ffd6c591a4cbadac43ebee8759db67
+size 6501
