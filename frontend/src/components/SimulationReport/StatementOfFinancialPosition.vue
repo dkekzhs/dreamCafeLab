fd0289118b5804d3b32bc31ca2e46203c34f85f6
kseenyoung <kseenyoung@gmail.com>
2024-04-02 02:37:17 +0900

    feat: 재무제표 그래프 매출만 선택 + 재무상태표 CSS
    Former-commit-id: 4433fba0cd9775d20c5793c0b61b6fa2dff22977 [formerly d2459dcdc1c582b01a021aae89b9398fdfd923d5 [formerly 91a612b59ff297234622db0926edccbae2c16119]]
    Former-commit-id: 457ecc89a56d324c70994f8ffb7d376ffabadaf0
    Former-commit-id: 1c8ba01aa7d52eeb696acc82721a1c7df333c142

--- frontend/src/components/SimulationReport/StatementOfFinancialPosition.vue
+++ frontend/src/components/SimulationReport/StatementOfFinancialPosition.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58275758a373858f25cfcc46d51d311dd80ecf954ecdc34ba7a06d5edca2cb7f
-size 2581
+oid sha256:66244070c84bd64af086564c7ca16e59147b145f39a2749999660c42d767cb22
+size 2946
--- frontend/src/components/SimulationReport/StatementOfFinancialPosition/IncomeChart.vue
+++ frontend/src/components/SimulationReport/StatementOfFinancialPosition/IncomeChart.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c62c05926e655be1520d15a558e24ca05e0d19b04db95be8ff9100ea6bc04bda
-size 2792
+oid sha256:afad73f073d00363dfe9bb5affcb8a3612f360a9c5d11a04b13f7ef862920ad2
+size 2959
