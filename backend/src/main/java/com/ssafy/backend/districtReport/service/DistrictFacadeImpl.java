63695608f185b96d1e5b343de2bc2d27c4b0b6e9
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-01 23:02:29 +0900

    feat : 추가 위치 기반 가장가까운 상권 3개 리스트 가져오기
    Former-commit-id: e9682ff48145b40308e4768619ad24b8df5b2446 [formerly caf92848dcb6ea8eac3825cb1d8fa38cec267d7d [formerly af2a3518c94e51a56272bcde9aa2bf3aa52d8c12]]
    Former-commit-id: ed5b37cac720f94330fcbbffacaeb5637c17b048
    Former-commit-id: 07b948585a95d3c202503651b52c7cc76b4d3e05

--- backend/src/main/java/com/ssafy/backend/districtReport/service/DistrictFacadeImpl.java
+++ backend/src/main/java/com/ssafy/backend/districtReport/service/DistrictFacadeImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e71965b741eb0cfd700791b880ba0f97b7d4421be908545c715ecf7bc3470867
-size 2744
+oid sha256:6002cae3a6998c19ef21dc0102ef000fb0f9edd5f14154735b076b81244bf39e
+size 3918
