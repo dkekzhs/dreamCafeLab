b0f98e7c5da5512481547de02adb5284538c102c
kseenyoung <kseenyoung@gmail.com>
2024-04-01 15:33:57 +0900

    feat: (가게정보) 편의시설 분포 그래프 데이터 연동
    Former-commit-id: 36a57e7f45c1ba9623a23281a5e362ff3bbe3271

--- frontend/src/components/SimulationReport/StoreInfo/FacilityChart.vue
+++ frontend/src/components/SimulationReport/StoreInfo/FacilityChart.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61d298c22e117a0ca9c4cf665468cf34a82ee595b871843184535b2080516a1d
-size 1956
+oid sha256:10aeaaa9cef41330cf564c46cfbca32e981ee9dc0652f53f3069406db1254f37
+size 2014
--- frontend/src/stores/simulationStore.js
+++ frontend/src/stores/simulationStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228c709fd1c41d7fcd34ba88cbcda4351429830afff4da001a78c87b66ebcaf8
-size 20675
+oid sha256:27cdf71df82f093eeb7cc3e6fb1b1933dbba5d3f312c0c0a7f9530ecddba5630
+size 119169
