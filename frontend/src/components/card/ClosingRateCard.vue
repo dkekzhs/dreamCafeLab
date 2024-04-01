d2d7a13eef1694c6857c266b0835f97433004cd7
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-02 01:50:16 +0900

    style: 폐업률 보고서 css 수정
    Former-commit-id: 039dc96811f84e3ef37f7d88cc1bc8871498a144

--- frontend/src/components/card/ClosingRateCard.vue
+++ frontend/src/components/card/ClosingRateCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8389979b4ba45096e7fda36a97b7c640185988fb1dbf1cdec939244a223b68ae
-size 7771
+oid sha256:0d10d8fdd274e12b92b2c0d513f7e1f86ee688e7f62643cfc037f6e6a69d292a
+size 8337
