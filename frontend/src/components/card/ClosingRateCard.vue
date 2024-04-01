6e01c091033fff90f8c6776289e42825ce90ee3a
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-02 02:21:04 +0900

    style: 매출 보고서 css 수정
    Former-commit-id: e8aed126b30f27bd9391f1acdaf58c7ca8bb340a [formerly 034146f35dcb1badf92c416cf494d2bdfb74d1ed [formerly 1ce1810e59d9a44ed29b647a31137421572d4076]]
    Former-commit-id: a1abc84d6faf507bed0c98738760564408817253
    Former-commit-id: 326dfe9bce8d03ecd40ca7b07f910b90b76b5505

--- frontend/src/components/card/ClosingRateCard.vue
+++ frontend/src/components/card/ClosingRateCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d10d8fdd274e12b92b2c0d513f7e1f86ee688e7f62643cfc037f6e6a69d292a
-size 8337
+oid sha256:450d93f76bf1bdbbb921afff920c05bed83fc9a248a4dc6d142599c515999652
+size 8355
