1625d9b91254edc2ce068d42fdc805442c7aae7e
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-01 21:12:36 +0900

    feat : 추가 상권 코드로 1,2,3 추천 브랜드 가져오기
    Former-commit-id: b71fdbc85d1504ab0505f99a12b8dbb43f19d611 [formerly 9894dfc410a60d513cb646a2a39e598c45086ef1 [formerly 86b4ccf42ee35b9eb1928f5ae9a96b213118a554]]
    Former-commit-id: 71e6dbf73a2ec985b3ca988c2f07320fdfc6e6a4
    Former-commit-id: 4c1ae2073c70156b03a2a88c4ffba633f4853764

--- backend/src/main/java/com/ssafy/backend/districtReport/controller/DistrictReportController.java
+++ backend/src/main/java/com/ssafy/backend/districtReport/controller/DistrictReportController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6174e917082dad36cdf94b6934c7008e151dca35e84cdc9bbba2652dc1ddfb5a
-size 1393
+oid sha256:4c4cbf93db8e8be9b90baac361be8bd12f703a1e15714d4bac1d8711999779fc
+size 1719
--- backend/src/main/java/com/ssafy/backend/districtReport/service/DistrictFacade.java
+++ backend/src/main/java/com/ssafy/backend/districtReport/service/DistrictFacade.java
@@ -1,5 +1,6 @@
 package com.ssafy.backend.districtReport.service;
 
+import com.ssafy.backend.cafe.model.domain.CafeBrand;
 import com.ssafy.backend.cafe.model.dto.MapCafeInfoRequestDto;
 import com.ssafy.backend.districtReport.model.vo.MapDataVo;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface DistrictFacade {
 
     List<MapDataVo> getMapCafeInfo(int code);
+
+    List<CafeBrand> getRecommendBrands(int districtId);
 }
