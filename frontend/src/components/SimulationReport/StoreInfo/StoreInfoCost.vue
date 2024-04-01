c2cc9ca9e92d1cd210f7cf67a48d229b6acd5117
kseenyoung <kseenyoung@gmail.com>
2024-04-02 01:38:00 +0900

    feat: 기타 비용 비율 제거
    Former-commit-id: 3df21e7f67b291cdab2f949bf0c023c674fc7947 [formerly 190286ce651317180384d04e23379646592c148a [formerly bd762fb2a2be4f97c8dc54b8a622429a06f0cb43]]
    Former-commit-id: 69238e6ed7edbb43d9e3a1912c2ade5458b7ffca
    Former-commit-id: 5bf0ab901dab71d0a432d587dc15d8ac06c60066

--- frontend/src/components/SimulationReport/StoreInfo/StoreInfoCost.vue
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoCost.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0b20a102127bfaae89ffe5cc642f172804b62dcd0756ee4f7440f614643041e
-size 1768
+oid sha256:10000ba0813d3497397ae1ba4883e7fb23ec0fc181b0e6177f33d3caf09931b4
+size 1688
--- frontend/src/components/SimulationReport/StoreInfo/StoreInfoFacilityChart.vue
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoFacilityChart.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d92822e87415d6f2001326c39f8395bf038308c44abc34a6f7628563e371c8d7
-size 2782
+oid sha256:4e296a6d4e2ca16c54ae34f16f3071b5da55a9c1e757db6034e609253fcf8ebe
+size 2105
