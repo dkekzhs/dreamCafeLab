e2c64006d72e8fe0b9b033c33768dc7a28783105
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-02 01:06:09 +0900

    ..
    Former-commit-id: 83ef1c033ef040e0fe35cd13c9827785768451e5

--- frontend/src/js/simulator/menu/overlayMenu.js
+++ frontend/src/js/simulator/menu/overlayMenu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:889c5db32cbee4c18251f05ecf4269ebc1f7534a95c31e548dbaeab3f2657a8c
-size 11485
+oid sha256:34139c9d817f7043314d0af3c219c4b6f0b1ed53d95ba6db6c2e58737cdf96aa
+size 12644
