39988e016f7d47c52d996c97b44bb1fa6948df86
dkekzhs <dkekzhs5@naver.com>
2024-04-02 18:46:46 +0900

    fix : 분기는 달 단위
    Former-commit-id: 77c7844631a4614f948a48a0891646bba76392bf

--- backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
+++ backend/src/main/java/com/ssafy/backend/simulation/service/SimulationServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4e17f9628b325493343c04f4e32e1bb38c97cb2b1ccdbe222e08e11e7460da5
-size 11425
+oid sha256:ea13678e58b25f44b23eeb74cc993e0248d436e9dc109ab14c45e96b77386dea
+size 11421
