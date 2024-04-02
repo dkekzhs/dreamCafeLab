819d4174a3f25bcda41677c9772a9e1dfc1d401e
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-02 10:49:58 +0900

    feat : fe 소중대 평형 입력 값 수정
    Former-commit-id: 9163fd9b36376fdaf1ce683ac6285904115fb857

--- frontend/src/views/SimulationReport/SimulationAsset2.vue
+++ frontend/src/views/SimulationReport/SimulationAsset2.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9510c54223f00dcb9637d1615573209b9dc790f870ab5ded7772ac426fcf5fcd
-size 3446
+oid sha256:eed1f5207fd607c0c705e96253e4f26c7bd613951de9c45d3ec3df91c169fa12
+size 3430
