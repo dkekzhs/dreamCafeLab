66fee7efdcee0997df7332be8c0c2035e8a4db6e
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-03 08:39:21 +0900

    style: card page 1차 css
    Former-commit-id: faaab1f02c85343f6d57297cddb5f6195edf9708

--- frontend/src/components/card/Card.vue
+++ frontend/src/components/card/Card.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccae4a3a8031b2564f29774f8cf26e4e032cff506e77ee6f292044f464834095
+oid sha256:2b3ca7f45447feea7fe121f441c1c43fdfd4f82c29e3059c969e26b97d4b02eb
 size 1598
