677454e7bbf3462839457d7ee906dcadeb4174d4
kseenyoung <kseenyoung@gmail.com>
2024-04-02 23:23:41 +0900

    feat: 뒤로가기 버튼을 포함한 헤더 생성
    Former-commit-id: ce2668019ac38ed44bb9bd0c7107f6fe5675d26b [formerly 6c8267be07bdda112dda0ffad0bc660bcd6087e9 [formerly 7062689f483d169d381dfb024eb2a8cee06d7d76]]
    Former-commit-id: 020abc2bbccc8c74fc4c2ff92f6067c11aa09477
    Former-commit-id: d105d8a696437ba65d11842a26a20720494ed743

--- frontend/src/views/SimulationReport/SimulationPolyFileView.vue
+++ frontend/src/views/SimulationReport/SimulationPolyFileView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:298dbcfca02f4f25ac4254a94eb6d76ad115f5710e079e1cf89f33f32daef94b
-size 5120
+oid sha256:9a75bc0f43f1145e670272bcd9ea59cfa25a73b67cffe17995895004be634101
+size 5063
--- frontend/src/views/SimulationReport/StoreInfoView.vue
+++ frontend/src/views/SimulationReport/StoreInfoView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ed85d9b60adc2cbacdef0814f0ef6c507d22ec873524df4dfe38cd7670557e1
-size 5035
+oid sha256:21a34c56d1b9c2d168ec72b5838a23beccd9b0cc9e3911f691a4d8544a2dcf5e
+size 4896
