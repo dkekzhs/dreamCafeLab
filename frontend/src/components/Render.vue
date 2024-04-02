039b40d91568dd10fecb0da933a3645081e3d2bf
김다인 <frambo1se@naver.com>
2024-04-02 09:15:50 +0900

    feat: 시뮬레이션 시작 api 연결
    Former-commit-id: 31812ed7c8a2037fbd0494b907d0f14e6f0da62a [formerly 3fe2c3d02582c4c1de57f5e0adfe6580221b65b1 [formerly b1ce895f033c805ef2a574253aec91292d456d8e]]
    Former-commit-id: 4f18f8b7cdebea271820140209ebf32eff3d8080
    Former-commit-id: 1cf87ab2e3f2fc657aaf5c4bbd145ac138eb6c5a

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d88fd9e4816ff25c750ba2b4fbc296a3dc772f8b46c5f93c62e377d231ec1cf3
-size 1856
+oid sha256:c369d342f3bd60ba3e769da0ded611fa86c51f78bd1d8c6b91d3ef1b86a77a62
+size 4010
