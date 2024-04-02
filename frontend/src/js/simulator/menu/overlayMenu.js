d83ff97ffb85fa6e99494283d0144d6f185692ce
김다인 <frambo1se@naver.com>
2024-04-03 01:19:09 +0900

    feat: 시뮬 종료시 updateLog simStore에 저장 / beforeRevenueList, latestRevenueList 관리
    Former-commit-id: 9275c79ed565eb1904814286231dcc07f27f20ea [formerly 500f8f131d6194e5637b2b52891684e4ff14b492 [formerly e29dd27554a41fd9c6799f00690392c99089f0ee]]
    Former-commit-id: a551e818ae863c976afa3aec69699425d71f876a
    Former-commit-id: b7af2dad0816fdb67da69fe093c3b2417ae1502c

--- frontend/src/js/simulator/menu/overlayMenu.js
+++ frontend/src/js/simulator/menu/overlayMenu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eeb93a0706f87819b57c0d9cf917f9bcc9400e0a0239b1e79b685586909f622
-size 14495
+oid sha256:07b859aa67fe756ed8b050895221bb8fd9c1d0b3a8c90673db436f9cb82c90b1
+size 14618
