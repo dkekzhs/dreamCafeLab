a89da00f82264b9c95b1e7389f0319b18f53b14d
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-03 06:54:45 +0900

    feat : 시물레이션 지도 수정중
    Former-commit-id: d233d063c994c22da7edb844a8a615ffe66b8a95

--- frontend/src/views/SimulationReport/SimulationMap.vue
+++ frontend/src/views/SimulationReport/SimulationMap.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:262e6716daf2d2bec752c97043079afbf30f583e64d28cd268c45f1f0d3eaf78
-size 4885
+oid sha256:769e04bb1390e0cef8c2e2e547d5d4a1a57832530c594311b8c8e3725e0f3d80
+size 14111
