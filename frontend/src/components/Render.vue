583b5b68d5c65d7aa6bd4de438a0a3ac100e0c16
김다인 <frambo1se@naver.com>
2024-04-02 13:22:34 +0900

    feat: 시뮬레이션 중 업데이트 시 api 호출, 결과값 store에 업데이트
    Former-commit-id: e486efa204438f769ffd85e1c704101a91cf00bb

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f8d8c5261fd2829168a5987c5e2ac2576bec2340b235a02a0811e8eb834b202e
-size 3267
+oid sha256:cb3c8208f87df8212c4d6e808e09ca5a9994671f5299dc4ae7056b3df741598c
+size 3228
