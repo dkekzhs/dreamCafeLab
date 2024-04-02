833a2c1f4b3296e4b29f0343e261603dff7fda97
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-02 13:49:13 +0900

    style: brand 선택 css
    Former-commit-id: f18accf375faa30b7d217b9aa236def573d879a6

--- frontend/src/components/card/SalesCard.vue
+++ frontend/src/components/card/SalesCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfc04bacde0f3516f1e9ea2b71bbd43ed3a71e9a02ea5540b17d8081f3e958bd
-size 9637
+oid sha256:c8434c6023d7b9a1741c84f471d6705246fb090bcab540c0c2a7613876c82dd0
+size 9883
