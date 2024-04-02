500f8f131d6194e5637b2b52891684e4ff14b492
김다인 <frambo1se@naver.com>
2024-04-03 01:19:09 +0900

    feat: 시뮬 종료시 updateLog simStore에 저장 / beforeRevenueList, latestRevenueList 관리
    Former-commit-id: e29dd27554a41fd9c6799f00690392c99089f0ee

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb3c8208f87df8212c4d6e808e09ca5a9994671f5299dc4ae7056b3df741598c
-size 3228
+oid sha256:e6ed60d9d55bce662736687b96bb2f8cb1f25985d619fea74f5431bd4d475db6
+size 3086
