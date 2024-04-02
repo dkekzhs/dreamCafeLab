77b778fefd6ada488330cd2592418defbc326a77
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-02 11:06:04 +0900

    fix : fe cafeSize, employees 입력 오류 수정
    Former-commit-id: 91d33da516314330a1dd36cbd2d5a066dec2f539 [formerly f34d28755b7948f1870d46d8df57db55682a9efa [formerly 00d1beb0e8931cfd88b24f969fcca1dfa1591c13]]
    Former-commit-id: 6c187b4e24faedabc17dca13872912fa66dd329a
    Former-commit-id: 57eb1053dbbcb904792f23b3014160f7a1074e5b

--- frontend/src/stores/dataStore.js
+++ frontend/src/stores/dataStore.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dbd15ea131d9bc7f679e5d63cee5b3cb41c91460a7896a38e736069b10369dd
-size 1027
+oid sha256:ee54b7d8e8054993b7e8da3b3781fe3619b0678ef668ca70b34e366675134d15
+size 1017
