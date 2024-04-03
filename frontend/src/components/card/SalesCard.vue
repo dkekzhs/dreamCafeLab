752795bd24468a3e0f7e09dffc542c73db3a22de
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-03 23:03:50 +0900

    feat : 카드 보일때 애니메이션 추가 하지만 제대로 동작안됨
    Former-commit-id: 381781167d0197a15d40ee319b9d5e1977036625 [formerly 1d013cd39fe7daf9022b6a1b2734873995c7136d [formerly 1238555aaca93ed3dfb3865653dcd82b7307cda1]]
    Former-commit-id: ff1b322ca7585d645ab93fe7d6ec5ca1f6fdbda0
    Former-commit-id: d92356d17452e482424517b8eb6b844aba17c988

--- frontend/src/components/card/SalesCard.vue
+++ frontend/src/components/card/SalesCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8321617f4227aa085a42f7abb7cde2a9dafbd3de35de654cb6153ae70127f7da
-size 9855
+oid sha256:7ff523f6d1c64d9b46bb4f1e2cb43f4d4ab87a1df8016e999e7d2f0935301082
+size 9970
