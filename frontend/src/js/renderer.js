6f18c279c95abb4003faddb252f306333c3596c6
김다인 <frambo1se@naver.com>
2024-04-03 16:40:12 +0900

    feat: 직원, 집객시설 추가/삭제에 따른 toast 띄우기
    Former-commit-id: 914e3ad2b446d57d892688630e54b84c6105c5ea

--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a55df30f38d2f449c5de1bde2908fdfc2c2898f5c3b055aa303d20e998013e8c
-size 8869
+oid sha256:4b88453b6dd450020ad5e72a9f6bd20296856cb33c8188a8a36aee10c64596b2
+size 8942
