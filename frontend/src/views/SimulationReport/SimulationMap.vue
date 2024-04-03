5afd9bd6ac8d18efc7b738c0aa8361420a15206f
dkekzhs <dkekzhs5@naver.com>
2024-04-03 15:38:27 +0900

    fix : 상권분석보고서 화면 비율
    Former-commit-id: 20189683a7a7ffc5a75bfb90b774dba3f89ccc92 [formerly a7426f344e37f1a652fbe6c0d66ccb85f37840f6 [formerly 2346829f964d971d1c7c6e7466b62965335426f6]]
    Former-commit-id: c9c1dc9b256e9ebe8d6d9e78dd0b2faef9864aad
    Former-commit-id: a034c82d6cc3214199d3877dd6539e342d36b895

--- frontend/src/views/SimulationReport/SimulationMap.vue
+++ frontend/src/views/SimulationReport/SimulationMap.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8776bd0cd7d82db952352d58565d048cd2fef5d5fb36d159fc77e4ccd467026f
-size 8855
+oid sha256:b51761f993edf923e95530030de562a297127138c6e537288274d2941db9adc1
+size 9042
