7ee8047a41c22d3807f43edd3f26762928c406b0
dkekzhs <dkekzhs5@naver.com>
2024-04-03 10:22:58 +0900

    fix :  시뮬레이션 선택 시 이름뜨게 수정
    Former-commit-id: fecb35d6054824c6a6f7452735ed7592903f2d6c [formerly 02fa9a5ce02ac80a18900bfb62872d8c3a6b4ec2 [formerly 47968c8ef61f3aac90d61a859735c816dac2379c]]
    Former-commit-id: 39446b59ec779d6b611494d34601bbe5cf01b841
    Former-commit-id: a71463edd8d7de218e1f7d87c355759f934bdf13

--- frontend/src/views/SimulationReport/SimulationMap.vue
+++ frontend/src/views/SimulationReport/SimulationMap.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25d810b456d93828076626247b215646c39d8679c8e86e27cc637d7985fe0c3c
-size 14118
+oid sha256:8776bd0cd7d82db952352d58565d048cd2fef5d5fb36d159fc77e4ccd467026f
+size 8855
