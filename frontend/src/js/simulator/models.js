494f5b80ab0c0bda53b19deb92ebe99015dde30a
김다인 <frambo1se@naver.com>
2024-04-03 16:40:12 +0900

    feat: 직원, 집객시설 추가/삭제에 따른 toast 띄우기
    Former-commit-id: 1dcbd37edca579cc0aeacd7a9167f5d525eeed1e [formerly 6f18c279c95abb4003faddb252f306333c3596c6 [formerly 914e3ad2b446d57d892688630e54b84c6105c5ea]]
    Former-commit-id: 7e5147f6a41805b257158883bac56c6cf27eee01
    Former-commit-id: 3fcf978367d26447109db7ef2c3f558fc531ba13

--- frontend/src/js/simulator/models.js
+++ frontend/src/js/simulator/models.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54b0e5b160a5952e8a5a8142a392bf9fac2c23614ef9ce20bd90cb21b5543bfe
-size 3658
+oid sha256:eef70c53a05fa81502953723fe92cf95a5f7ae1e89ecfc37f3ea2d90a19f7c3e
+size 3816
