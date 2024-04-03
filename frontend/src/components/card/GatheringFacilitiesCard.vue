8ed993a711e6807d5e299484fd03cf329018298a
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-03 17:20:16 +0900

    style: 보고서 css
    Former-commit-id: 4ed873e420fc98a081b1caf8d98db8f4c48b71c8

--- frontend/src/components/card/GatheringFacilitiesCard.vue
+++ frontend/src/components/card/GatheringFacilitiesCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e791c8409ddc882ca9e26caca189cbfa96dab0adce53df2cd5b5c6d252ddf5af
-size 8753
+oid sha256:491121f47f11c30f8df57a0c05ca015adb2dfb1c33a68f00f24d7df06061806e
+size 9992
--- frontend/src/components/card/SalesCard.vue
+++ frontend/src/components/card/SalesCard.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ef9be596b5979cf5607518d4b9b5adb852f29c70d51bcf01ac0debb6c77b14d
-size 10696
+oid sha256:dff4a75155c65fbcc535e4c3c6a2154eac79f201fac002d2de3bda1fc123bf9f
+size 9873
