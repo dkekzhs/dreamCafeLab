fd1a11aa490bf1a603f035d6572e6921a3a2fd94
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-04 07:09:13 +0900

    fix : 재화형이 잘못올린 브랜드 원래대로
    Former-commit-id: 104182081e4e37d55b93b8b9266744e3fbaf89e9 [formerly a364e9bdcb99abbd760f8f308222e4a0e8c2a09c [formerly cffca75749fae54cb42d47ace7dcc1c940a7a08a]]
    Former-commit-id: dc9853f57340820da9cb3e7240cf3a513cc5f7c0
    Former-commit-id: 4980c46399ba3d062bbf315ea70c536cd32fdcc9

--- frontend/src/views/SimulationReport/SimulationBrand.vue
+++ frontend/src/views/SimulationReport/SimulationBrand.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b281d70f88a4a1c6b20eba4a9ce9f7f1344902a0b731887e2bb5259cd5a17f4d
-size 9559
+oid sha256:4e4752552288f1a6871a77cf3f48bebb19d1a19c24197b76f04f142b4e550637
+size 9615
