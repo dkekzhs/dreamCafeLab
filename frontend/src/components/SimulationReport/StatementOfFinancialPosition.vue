fedfb2ab62ff8d8610fa8a1c99353802eb1c631a
kseenyoung <kseenyoung@gmail.com>
2024-04-04 06:12:30 +0900

    feat: 손익계산서 계산 수정
    Former-commit-id: 037b6fcb962fc7844680d6767342ccd981267a68

--- frontend/src/components/SimulationReport/StatementOfFinancialPosition.vue
+++ frontend/src/components/SimulationReport/StatementOfFinancialPosition.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58d4a54d21143b33b046c267724f938ed56a1d787c6dfb2ae6fe3941ca729a52
-size 2987
+oid sha256:3d371fd7aa8b9af596200f4dee087c2865ef62b30a44a47d4153aaa8bd9a32fe
+size 3024
