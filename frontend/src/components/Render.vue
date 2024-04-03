48f6a17b42bfa64e7103bdf96c0c83dc225b8d30
ggramgyo <woghk6761@pusan.ac.kr>
2024-04-04 01:09:06 +0900

    fix: animation 해결
    Former-commit-id: 6d9880ed4932bfe779878794c8c2c44e74244262

--- frontend/src/components/Render.vue
+++ frontend/src/components/Render.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2fbb4528e703441984bdb44a2e3df49a7bb736630cfd7da23dbaa02e163ea52
-size 3139
+oid sha256:aeefed43e7bfa11c3e9d4e12130248930f0f695b8f17ba608b3a4b8d6d1b009c
+size 3195
