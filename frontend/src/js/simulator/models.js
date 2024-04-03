cae4427f544bfcba8e6be0b2d344a7194ecad404
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-04 01:03:57 +0900

    feat : fe 에셋 수정
    Former-commit-id: bd19b28f163ff67094ed1bfe15dc9c48019d5dd0

--- frontend/src/js/simulator/models.js
+++ frontend/src/js/simulator/models.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92b1e61510400546fd5030d67d1180020d736a6a5c97ad47703b8e38ffbd72d5
-size 5386
+oid sha256:7c74d59256fb4557e2103c261509ca74eea30c1a00c7eac6ec11f9f5dd39ef7f
+size 5182
