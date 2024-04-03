825efe68ddf33a0abac95e47ce5e3e3c351b86f7
김다인 <frambo1se@naver.com>
2024-04-03 10:41:56 +0900

    feat: 집객시설별 asset 파일 경로 연결
    Former-commit-id: 8fb1a9c773d1b49ef1cf0118be75f00e1cd0b232

--- frontend/src/js/simulator/models.js
+++ frontend/src/js/simulator/models.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37aa9c7527fb66abb097c279a5b5c2aa7d2781dffa5f49e344df5b7732a75088
-size 2765
+oid sha256:54b0e5b160a5952e8a5a8142a392bf9fac2c23614ef9ce20bd90cb21b5543bfe
+size 3658
