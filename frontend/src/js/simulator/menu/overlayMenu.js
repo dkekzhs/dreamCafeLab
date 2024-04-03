624570f26baa406b12b49bfbc7c8d5b3c7e96622
louis-cho <38391852+louis-cho@users.noreply.github.com>
2024-04-03 23:25:16 +0900

    fix : 초기 일시정지 버튼 이미지 & 동전 효과음 교체
    Former-commit-id: b5366ce7a7aad943b652a2616c2389bc46de9d53

--- frontend/src/js/simulator/menu/overlayMenu.js
+++ frontend/src/js/simulator/menu/overlayMenu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94964495cf52e65abf205ed072b91b4fcddbd814dbf619781e67f82823cf6416
-size 14920
+oid sha256:42c7f0ff27527973fd2296319539b4a3d635035840a15c322b4caeee096e5e07
+size 14921
