e59a881b86a1901755d817b67543c840cd34a474
kseenyoung <kseenyoung@gmail.com>
2024-04-03 21:54:07 +0900

    fix: (가게정보)브랜드정보 한 개의 드롭다운만 가능하도록 수정
    Former-commit-id: 43af47546c6f3016a629bdd91e0b3125d9a336e0 [formerly 72f664d52a249e0fd278cf4a58c00e6e24c65d7a [formerly c6aa89977666fe22ba40ede98dd259e12316f532]]
    Former-commit-id: 69412309b62bec6fc2e14ca9c878d9775148a848
    Former-commit-id: 7ec21daac60693ad7a520e9ef0838c4a5a2010b2

--- frontend/src/views/SimulationReport/SimulationPolyFileView.vue
+++ frontend/src/views/SimulationReport/SimulationPolyFileView.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8bc01d81252d687ef43f22e39944a970102e598b9c68d2e18bfea8aab0eb2f3b
-size 5947
+oid sha256:380fb190c9443221b053a17a2cc771234b15fb2dde078512ff63eba1173409d2
+size 5974
