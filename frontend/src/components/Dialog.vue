2ff646eb564493763d59744f5d457d0bd6ab9038
김다인 <frambo1se@naver.com>
2024-04-04 08:53:14 +0900

    fix: 시뮬 시작/종료 dialog 내용 추가
    Former-commit-id: 65154e374ede1c6f15509887a01a3f8fab59972b

--- frontend/src/components/Dialog.vue
+++ frontend/src/components/Dialog.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64723f65814471b3d74a2be8f51f8547dc2f4400093482adfb2f651046c771fd
-size 3597
+oid sha256:914336317e744f26a71eaaf89ef826397f492d99e5fdb2e28e8b58d8ca5f62cc
+size 4158
