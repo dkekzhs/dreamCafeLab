f397093d89004ef92e8f0bf534e1ec90bfac6a8d
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2024-04-04 07:33:43 +0900

    fix : 배포할 떄 쓰도록 패스 변경
    Former-commit-id: d382734c057869861f01cd4bdb1954a6f191a6c2 [formerly 8a1c032f96339b6cc6e0a2745fa945b3eb84f195 [formerly cd36582a53de1e38a9dc6b91ca0e703c98e9ab55]]
    Former-commit-id: e2a0b56fd08870495527c28d4f4d12d8ff36cc68
    Former-commit-id: abbae76836fdbec450697b27d9f62424d0194449

--- frontend/src/components/SimulationReport/StoreInfo/StoreInfoBasic.vue
+++ frontend/src/components/SimulationReport/StoreInfo/StoreInfoBasic.vue
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5475018caa71d09fbea9dcb53f8e7474e9f9f00cf97af031c421cec30a2bfe5
+oid sha256:9ea5fe4ae9472f3c162135712d3845b1285d9a2dff6dab1603d2a373f9176942
 size 3958
