43e739cf0cd6a18d1039746af1e924c58b3cd943
김다인 <frambo1se@naver.com>
2024-04-04 11:35:39 +0900

    fix: 잔고에 원 붙임
    Former-commit-id: c9edcb84f9bd4b29b6bcb68484d8e4827fef7ee0

--- frontend/src/js/simulator/menu/overlayMenu.js
+++ frontend/src/js/simulator/menu/overlayMenu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:073b30dd39b928a99b00384b51b6687d5fbdc0d4b4a316722a09e619bdd2c095
-size 15362
+oid sha256:07df2e9dfa3bd25bdb4e00bbaa8853af4af9e516debcd089f89b2f3de38bc7e7
+size 15377
