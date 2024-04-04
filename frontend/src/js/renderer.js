953a2c1c91f7001ff2a553d28c2135727a83cbdb
김다인 <frambo1se@naver.com>
2024-04-04 11:48:52 +0900

    fix: toast에 콤마넣기
    Former-commit-id: 76040ae8c23722ee767970be5fe682cded9f4238

--- frontend/src/js/renderer.js
+++ frontend/src/js/renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cf1900e07fef3501592a55f8ab6cf3d144d5b82a85d4bfdd98b4064bea81ab28
-size 10122
+oid sha256:526a75c7b9033256ba24cf8ac0028ac6ac75a67cefe8c5d858dc4eaa8a458688
+size 10269
