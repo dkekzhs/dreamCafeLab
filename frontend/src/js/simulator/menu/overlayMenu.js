413b33bcb811d6090daed1d2c475f8b88306e5b4
신시원 <ssw4627@gmail.com>
2024-04-04 10:48:13 +0900

    refactor: 오버레이 인포 수정
    Former-commit-id: 8768acc232dba5f859202f033dd4ffd083694dbc

--- frontend/src/js/simulator/menu/overlayMenu.js
+++ frontend/src/js/simulator/menu/overlayMenu.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa7ebbd506c57cec16dcd025f6fd5fa2adb008d05748cdef65880f3ade6e394
-size 14918
+oid sha256:073b30dd39b928a99b00384b51b6687d5fbdc0d4b4a316722a09e619bdd2c095
+size 15362
